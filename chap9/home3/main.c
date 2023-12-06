7251439e7201a62980a30997e5591d694ae0b101
mundi207 <alsrb6008@gmail.com>
2023-12-07 00:17:21 +0900

    plz

--- chap9/home3/main.c
+++ chap9/home3/main.c
@@ -1,29 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
-
-#define MAX_SIZE 100
 
 int main(void) {
-	char buf[MAX_SIZE];
-	char *argv;
-	int argc = 0;
+	char argv[30][30];
 	int i = 0;
+	int j = 0;
 
 	while(1) {
-		printf("[shell] >> ");
-		while(1) {
-			buf[i] = fgetc(stdin);
-			if(buf[i] == '\n') {
-				argv = buf;
-				if(strcmp(argv, "\n") == 0) {
-					argv[0] = '\0';
-				}
-				printf("%s", argv);
-				break;
-			}
-			i ++;
+		argv[i][j] = fgetc(stdin);
+		j ++;
+		if(argv[i][j] == '\n') {
+			break;
 		}
 	}
-	exit(0);
 }
+

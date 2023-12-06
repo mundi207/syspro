e4cad4ce7c7a077e54f2cadbbce0464d99fac39f
mundi207 <alsrb6008@gmail.com>
2023-12-07 00:24:26 +0900

    plz

--- chap9/home3/main.c
+++ chap9/home3/main.c
@@ -1,17 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 int main(void) {
-	char argv[30][30];
+	char *buf;
 	int i = 0;
-	int j = 0;
 
 	while(1) {
-		argv[i][j] = fgetc(stdin);
-		j ++;
-		if(argv[i][j] == '\n') {
-			break;
+		buf = (char*)malloc(sizeof(char) * 1);
+		printf("mundi@mundi >> ");
+		while(1) {
+			buf[i] = fgetc(stdin);
+			buf = (char*)realloc(buf, sizeof(char) * strlen(buf));
+			if(buf[i] == '\n') {
+				buf[i + 1] = '\0';
+				system(buf);
+				free(buf);
+				break;
+			}
+			i ++;
 		}
 	}
+	exit(0);
 }
+
 
